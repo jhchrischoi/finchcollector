3f3f6575a1bc885872ec2f7db64e2ee90c97cbda
jhchrischoi <jhchrischoi@gmail.com>
2023-05-17 23:10:12 -0500

    lab part 4 1:M relationships complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 MEALS = (
     ('F', 'Fruit'),
@@ -15,4 +16,23 @@ class Finch(models.Model):
   
     def __str__(self):
         return f'{self.name} ({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
 
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+  )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from.models import Finch 
-from django.views.generic import ListView
+from.models import Finch
+from .forms import FeedingForm
+from django.views.generic import ListView, DetailView
 
+class FinchList(ListView):
+  model = Finch
 
-# Define the home view
 def home(request):
-  # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
-class FinchList(ListView):
-  model = Finch
+def finches_index(request):
+    finches = Finch.objects.filter()
+    return render(request, 'finches/index.html', 
+    { 
+        'finches': finches 
+    }
+)
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'main_app/detail.html', { 'finch': finch })
+  feeding_form=FeedingForm()
+  return render(request, 'main_app/detail.html', { 'finch': finch, 'feeding_form':feeding_form })
 
 class FinchCreate(CreateView):
   model = Finch
@@ -31,3 +38,14 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
